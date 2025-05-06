987ff80bdd230c5bb6b00089f4ad869ec5923dde
biel101010e <00001125278092sp@al.educacao.sp.gov.br>
2025-05-06 08:49:00 -0300

    fiz um sistema para verificar se é maior ou menor de iade

---
+++ exercicio03.py
@@ -0,0 +1,8 @@
+def verificar_idade(nome, idade):
+    if idade >= 18:
+        return f"{nome} é maior de idade."
+    else:
+        return f"{nome} é menor de idade."
+    
+    
+print(verificar_idade("biel", 69))
